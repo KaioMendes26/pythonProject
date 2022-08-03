9195f614095d626b2a0436d4284382f78ea814a5
KaioMendes26 <kaio.mendes2609@gmail.com>
2022-08-03 17:58:37 -0300

    cuso lógica pyton

---
+++ crescente.py
@@ -0,0 +1,14 @@
+print("Digite dois numero: ")
+x = int(input())
+y = int(input())
+
+while x != y:
+    if x < y:
+        print("CRESCENTE!")
+    else:
+         print("DESCRESENTE!")
+
+
+    print("Digite outros dois numeros: ")
+    x = int(input())
+    y = int(input())
